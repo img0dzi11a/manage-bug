3276d758b78793fa45a7bd56daf29b90a4095e7a
img0dzi11a <89905432+img0dzi11a@users.noreply.github.com>
2024-09-25 02:35:37 +0500

    create-partial-for-new-project-form

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,34 +1,29 @@
 class ProjectsController < ApplicationController
     def new
-        # There will be error if project fails to save because there is no new.html.erb file 
+        
     end
     def create
-        @project = Project.new(project_params)
-        if @project.save
+      @project = Project.new(project_params)
 
-            developer_ids = params[:project][:developer_ids] || []
-            qa_ids = params[:project][:qa_ids] || []
+      if @project.save
+        developer_ids = params[:project][:developer_ids] || []
+        qa_ids = params[:project][:qa_ids] || []
 
-
-            developer_ids.each do |developer_id|
-              @project.users << User.find(developer_id)
-            end
-
-            qa_ids.each do |qa_id|
-              @project.users << User.find(qa_id)
-            end
-            redirect_to @project
-
-        else
-            flash[:success] = "Kindly fill the whole form"
-            render 'new'
+        developer_ids.each do |developer_id|
+          @project.users << User.find(developer_id)
         end
+
+        qa_ids.each do |qa_id|
+          @project.users << User.find(qa_id)
+        end
+        redirect_to @project
+      end
     end
 
     def show
-        @project = Project.find(params[:id])
-        @bugs = @project.bugs
-        @developers = @project.users.where(user_type: "developer") #developers associated to that project
+      @project = Project.find(params[:id])
+      @bugs = @project.bugs
+      @developers = @project.users.where(user_type: "developer")
     end
 
     private
